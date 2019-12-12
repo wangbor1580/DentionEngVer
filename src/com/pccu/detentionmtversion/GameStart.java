f1b4280c43d8e23388be12c9f888f7bd496a2732
WangBor <a7233780@g.pccu.edu.tw>
2019-12-12 15:02:44 +0800

    2-th Commit

--- src/com/pccu/detentionmtversion/GameStart.java
+++ src/com/pccu/detentionmtversion/GameStart.java
@@ -6,7 +6,7 @@ public class GameStart {
 		// TODO Auto-generated method stub
 		
 		IntelligenceCenter infoCenterA = new IntelligenceCenter();
-		
+	
 		Scene sceneA = new Scene() {
 			@Override
 			public Events getEvent(int cnt) {
